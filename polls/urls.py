97c7daa96a71b099eb7c44dad0a9248aa789ab45
lio <shun7018.cod@gmail.com>
2021-12-10 18:21:45 +0900

    first commit

---
+++ polls/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # path('', views.index, name='index'),
+
+    # ex: /pools/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>', views.detail, name='detail'),
+
+    # ex: /pools/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+
+    # ex: /pools/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+]
